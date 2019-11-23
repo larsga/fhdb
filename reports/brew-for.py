7351bcc37e2c57510672f7f0aa4f4fb56334ff31
Lars Marius Garshol <larsga@garshol.priv.no>
2019-11-23 10:55:20 +0100

    Brew for event histogram table added

---
+++ reports/brew-for.py
@@ -0,0 +1,33 @@
+
+import sparqllib
+import tablelib
+
+# ===== EVENTS BY COUNTRY
+
+query = '''
+prefix dc: <http://purl.org/dc/elements/1.1/>
+prefix neg: <http://www.garshol.priv.no/2014/neg/>
+prefix neu: <http://www.garshol.priv.no/2015/neu/>
+prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+prefix tb: <http://www.garshol.priv.no/2014/trad-beer/>
+prefix dbp: <http://dbpedia.org/resource/>
+prefix uff: <http://www.garshol.priv.no/2015/uff/>
+
+SELECT DISTINCT ?h ?c ?s
+WHERE {
+  ?s dc:title ?title;
+    tb:part-of ?c;
+    tb:brew-for ?h.
+
+  ?c a dbp:Country.
+}'''
+#  FILTER (?h != neg:alder-branches && ?h != neg:straw )
+
+def get_herb_name(h):
+    return tablelib.get_last_part(str(h))
+
+tablelib.make_table('brew-for.html', query, get_herb_name,
+                    label = 'events',
+                    caption = 'Brewing for annual events.',
+                    min_accounts = 3,
+                    format = 'html')
