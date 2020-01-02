c7d39c3d2f9342bf3541d1790e319fe05ea1506e
Lars Marius Garshol <larsga@garshol.priv.no>
2020-01-02 10:10:47 +0100

    More kinds of small beer names

--- reports/small-beer-names.py
+++ reports/small-beer-names.py
@@ -4,10 +4,12 @@ import re
 import maputils
 
 LETT = re.compile(u'lettøl')
-TYNN = re.compile(u't(o|y|u)n(n|t)øl')
+TYNN = re.compile(u't(o|y|u)n(n|t)(øl|dricka)')
+SPISS = re.compile(u'spis(s)?(ö|ø)l')
 
 symbols = [
     (LETT, '#FFFFFF', u'Lettøl'),
     (TYNN, '#00FFFF', u'Tynnøl'),
+    (SPISS, '#FF00FF', u'Spissøl'),
 ]
 maputils.make_term_map('tb:small-beer-name', symbols, 'small-beer-name.html')
