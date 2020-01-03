a324d758b7c6750c3b7e67fa5d06d735e4437585
Lars Marius Garshol <larsga@garshol.priv.no>
2020-01-03 10:13:57 +0100

    Spent grain term map

---
+++ reports/spent-grain-term.py
@@ -0,0 +1,14 @@
+
+#encoding=utf-8
+
+import re
+import maputils
+
+DRAV = re.compile('drav(a)?')
+MASK = re.compile(u'm(a|ei|e|ä)sk')
+
+symbols = [
+    (DRAV, '#00FF00', 'Drav'),
+    (MASK, '#0000FF', 'Mask'),
+]
+maputils.make_term_map('tb:spent-grain-term', symbols, 'spent-grain-term.html')
