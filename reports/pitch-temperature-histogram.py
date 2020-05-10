fea16ececac8ea492f152170cfb6ab8cbb7fc240
Lars Marius Garshol <larsga@garshol.priv.no>
2020-05-10 19:44:08 +0200

    Report category counts properly

--- reports/pitch-temperature-histogram.py
+++ reports/pitch-temperature-histogram.py
@@ -14,6 +14,7 @@ singles = 0
 ranges = 0
 milks = 0
 bodies = 0
+other = 0
 
 unreadable = 0
 temperatures = []
@@ -21,7 +22,20 @@ for (s, lat, lng, t, c) in sparqllib.query_for_rows(pitch.query):
     temp = pitch.get_temp(t)
     if temp:
         temperatures.append(temp)
+
+        cat = pitch.get_category(t)
+        if cat == 'single':
+            singles +=1
+        elif cat == 'range':
+            ranges +=1
+        elif cat == 'milkwarm':
+            milks +=1
+        elif cat == 'body':
+            bodies +=1
+        else:
+            other += 1
     else:
+        # print 'UNINTERPRETABLE', t
         unreadable += 1
 
 temperatures.sort()
@@ -29,10 +43,11 @@ print temperatures
 print 'Interpreted temperatures', len(temperatures)
 print 'Unreadable', unreadable
 print 'Average', average(temperatures)
-# print '  Single', singles
-# print '  Range', ranges
-# print '  Milk', milks
-# print '  Body', bodies
+print '  Single', singles
+print '  Range', ranges
+print '  Milk', milks
+print '  Body', bodies
+print '  Other', other
 
 from matplotlib import pyplot
 (n, bins, patches) = pyplot.hist(temperatures, BINS, alpha=0.5,
