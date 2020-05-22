b29a33a7a5726ad9985c7e95089029d7d21d3678
Lars Marius Garshol <larsga@garshol.priv.no>
2020-05-22 15:53:22 +0200

    Add map of herb usage

---
+++ reports/herb-map.py
@@ -0,0 +1,43 @@
+
+import sys, string
+import config
+import colorsys
+import maputils
+import sparqllib
+
+themap = config.make_map_from_cli_args()
+herb = sys.argv[2]
+
+white = themap.add_symbol('white', '#FFFFFF', '#000000', strokeweight = 1,
+                          title = string.upper(herb[0]) + herb[1 : ])
+black = themap.add_symbol('black', '#000000', '#000000', strokeweight = 1,
+                          title = 'No ' + herb)
+
+query = '''
+prefix neg: <http://www.garshol.priv.no/2014/neg/>
+prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+prefix tb: <http://www.garshol.priv.no/2014/trad-beer/>
+
+SELECT ?s ?title ?lat ?lng ?herbs
+WHERE {
+  ?s
+    dc:title ?title;
+    geo:lat ?lat;
+    geo:long ?lng;
+    tb:herbs ?herbs.
+}'''
+accounts = {}
+has_herb = {}
+for (s, title, lat, lng, herbs) in sparqllib.query_for_rows(query):
+    has_herb[s] = herbs.endswith('/' + herb) or has_herb.get(s)
+    accounts[s] = (title, lat, lng)
+
+for (s, (title, lat, lng)) in accounts.items():
+    if has_herb[s]:
+        symbol = white
+    else:
+        symbol = black
+    themap.add_marker(lat, lng, title, symbol)
+
+themap.set_legend(True)
+themap.render_to('herb-map')
