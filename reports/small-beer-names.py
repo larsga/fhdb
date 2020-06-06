c7382d85ffed86abe4198abab91389d6a5d3779f
Lars Marius Garshol <larsga@garshol.priv.no>
2020-06-06 17:56:26 +0200

    Add dricka

--- reports/small-beer-names.py
+++ reports/small-beer-names.py
@@ -7,11 +7,13 @@ LETT = re.compile(u'lettøl')
 TYNN = re.compile(u't(o|y|u)n(n|t)(øl|dricka)')
 SPISS = re.compile(u'sp(i|y|ø)s(s)?(ö|ø|e)l')
 SVAGDRICKA = re.compile(u'svagdricka')
+DRICKA = re.compile(u'dr(e|i)(c|k)k(a|e)')
 
 symbols = [
     (LETT,       '#FFFFFF', u'Lettøl'),
     (TYNN,       '#00FFFF', u'Tynnøl'),
     (SPISS,      '#FF00FF', u'Spissøl'),
     (SVAGDRICKA, '#FFFF00', u'Svagdricka'),
+    (DRICKA,     '#AAAA00', u'Dricka'),
 ]
 maputils.make_term_map('tb:small-beer-name', symbols, 'small-beer-names')
