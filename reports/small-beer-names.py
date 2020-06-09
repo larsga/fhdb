3514bfc3ca65150f718d3033de384e685ea718dd
Lars Marius Garshol <larsga@garshol.priv.no>
2020-06-09 18:13:27 +0200

    Add 'dagligøl'

--- reports/small-beer-names.py
+++ reports/small-beer-names.py
@@ -8,6 +8,7 @@ TYNN = re.compile(u't(o|y|u)n(n|t)(øl|dricka)')
 SPISS = re.compile(u'sp(i|y|ø)s(s)?(ö|ø|e)l')
 SVAGDRICKA = re.compile(u'svagdricka')
 DRICKA = re.compile(u'dr(e|i)(c|k)k(a|e)')
+DAGLIGOL = re.compile(u'dagligøl')
 
 symbols = [
     (LETT,       '#FFFFFF', u'Lettøl'),
@@ -15,5 +16,6 @@ symbols = [
     (SPISS,      '#FF00FF', u'Spissøl'),
     (SVAGDRICKA, '#FFFF00', u'Svagdricka'),
     (DRICKA,     '#AAAA00', u'Dricka'),
+    (DAGLIGOL,   '#0000FF', u'Dagligøl'),
 ]
 maputils.make_term_map('tb:small-beer-name', symbols, 'small-beer-names')
