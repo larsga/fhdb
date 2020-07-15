6fa0d46d50326fa7a804c66310ac6dc07698047e
Lars Marius Garshol <larsga@garshol.priv.no>
2020-07-15 10:16:56 +0200

    Map of where kvass was made

---
+++ reports/kvass-map.py
@@ -0,0 +1,32 @@
+
+import config
+import maplib
+import sparqllib
+
+themap = config.make_map_from_cli_args()
+
+symbols = {
+    '1' : themap.add_symbol('white', '#FFFFFF', '#000000'),
+    '0' : themap.add_symbol('black', '#000000', '#000000'),
+    'true' : themap.add_symbol('white', '#FFFFFF', '#000000'),
+    'false' : themap.add_symbol('black', '#000000', '#000000')
+}
+
+query = '''
+prefix dc: <http://purl.org/dc/elements/1.1/>
+prefix neg: <http://www.garshol.priv.no/2014/neg/>
+prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+prefix tb: <http://www.garshol.priv.no/2014/trad-beer/>
+
+SELECT DISTINCT ?s ?lat ?lng ?title ?mead
+WHERE {
+  ?s
+    dc:title ?title;
+    geo:lat ?lat;
+    geo:long ?lng;
+    tb:kvass ?mead.
+}'''
+for (s, lat, lng, title, mead) in sparqllib.query_for_rows(query):
+    themap.add_marker(lat, lng, title, symbols[mead])
+
+themap.render_to('kvass')
