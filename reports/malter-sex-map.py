8f634980f0dcb964d424cb98dd6cc27baffb7d4a
Lars Marius Garshol <larsga@garshol.priv.no>
2020-10-13 19:24:07 +0200

    Add malter-sex-map

---
+++ reports/malter-sex-map.py
@@ -0,0 +1,36 @@
+
+import config
+import sparqllib
+
+themap = config.make_map_from_cli_args()
+
+red = themap.add_symbol('red',   '#FF0000', '#000000', strokeweight = 1)
+pink = themap.add_symbol('pink', '#FF00FF', '#000000', strokeweight = 1)
+blue = themap.add_symbol('blue', '#0000FF', '#000000', strokeweight = 1)
+
+NEG = 'http://www.garshol.priv.no/2014/neg/'
+MALE = NEG + 'male'
+FEMALE = NEG + 'female'
+BOTH = NEG + 'both'
+symbol_map = {
+    MALE : blue,
+    FEMALE : red,
+    BOTH : pink
+    }
+
+query = '''
+prefix tb: <http://www.garshol.priv.no/2014/trad-beer/>
+prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+SELECT ?s ?lat ?lng ?sex ?title
+WHERE {
+  ?s
+    dc:title ?title;
+    geo:lat ?lat;
+    geo:long ?lng;
+    tb:malter-sex ?sex.
+}'''
+for (s, lat, lng, sex, title) in sparqllib.query_for_rows(query):
+    symbol = symbol_map[sex]
+    themap.add_marker(lat, lng, title, symbol)
+
+themap.render_to('malter-sex-map')
