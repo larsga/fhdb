74a3ad8b31c5ff8ccbdcf8c622cf88ea5152ca4a
Lars Marius Garshol <larsga@garshol.priv.no>
2020-12-31 15:43:03 +0100

    Update to new API

--- reports/mead.py
+++ reports/mead.py
@@ -1,21 +1,7 @@
 
-import colorsys
-import maplib
-import sparqllib
+import maputils
 
-lat = 61.8
-lng = 9.45
-
-themap = maplib.Map(lat, lng, 6)
-
-symbols = {
-    '1' : themap.add_symbol('white', '#FFFFFF', '#000000'),
-    '0' : themap.add_symbol('black', '#000000', '#000000'),
-    'true' : themap.add_symbol('white', '#FFFFFF', '#000000'),
-    'false' : themap.add_symbol('black', '#000000', '#000000')
-}
-
-query = '''
+maputils.make_boolean_map('''
 prefix dc: <http://purl.org/dc/elements/1.1/>
 prefix neg: <http://www.garshol.priv.no/2014/neg/>
 prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
@@ -28,11 +14,5 @@ WHERE {
     geo:lat ?lat;
     geo:long ?lng;
     tb:mead ?mead.
-}'''
-count = 0
-for (s, lat, lng, title, mead) in sparqllib.query_for_rows(query):
-    symbol = symbols[mead]
-    themap.add_marker(lat, lng, title, symbol)
-    count += 1
-
-themap.render_to('mead.html')
+}''',
+'mead')
