3c29a6340330e7c01766ae1a583760f273e2c0c7
Lars Marius Garshol <larsga@garshol.priv.no>
2021-07-22 13:13:15 +0200

    Country filter and l10n

--- reports/fermentation-time-histogram.py
+++ reports/fermentation-time-histogram.py
@@ -19,21 +19,37 @@ prefix uff: <http://www.garshol.priv.no/2015/uff/>
 SELECT DISTINCT ?s ?h
 WHERE {
   ?s dc:title ?title;
-    tb:fermentation-time ?h.
+    tb:fermentation-time ?h
+    %s .
 }
 '''
 
+if config.get_country():
+    f = '; tb:part-of dbp:%s' % config.get_country()
+else:
+    f = ''
+query = query % f
+
 times = [float(t) for (s, t) in sparqllib.query_for_rows(query)]
+
+(title, xlab, ylab) = {
+    'en' : ('Fermentation times', 'Hours', 'Number of accounts'),
+    'no' : (u'Gjæringstider', 'Timer', 'Antall beskrivelser'),
+}[config.get_language()]
 
 import numpy
 from matplotlib import pyplot
 pyplot.style.use(config.get_plot_style())
 
 (n, bins, patches) = pyplot.hist(
-    times, BINS, alpha=0.5,
-    label = 'Fermentation times'
+    times, BINS, label = title
 )
-pyplot.title('Fermentation times')
-pyplot.xlabel('Hours')
-pyplot.ylabel('Number of accounts')
-pyplot.show()
+pyplot.title(title)
+pyplot.xlabel(xlab)
+pyplot.ylabel(ylab)
+
+if not config.get_file():
+    pyplot.show()
+else:
+    pyplot.savefig(config.get_file())
+    pyplot.close()
